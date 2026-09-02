6a7cf541ec9b717fc347166093d2c96b36c9046a
kamal186-a <kamalsab8687@gmail.com>
2026-09-02 09:54:23 +0500

    Add script for downloading and streaming video
    This script downloads a video from Google Drive and streams it live to both Facebook and YouTube simultaneously.

--- stream.sh
+++ stream.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
+set -e
 
+# ---------- Configuration ----------
+GOOGLE_DRIVE_LINK="https://drive.google.com/file/d/1NxUZNHgVDpWMyFjH7c8V-XJyv8Da3GCB/view?usp=sharing"
+LOCAL_VIDEO_PATH="video.mp4"
+
+FACEBOOK_STREAM_KEY="FB-122193920732780799-0-Ab4JgxUm67HqqOAuoVJlmviL"
+FACEBOOK_RTMP_URL="rtmps://live-api-s.facebook.com:443/rtmp/${FACEBOOK_STREAM_KEY}"
+
+YOUTUBE_STREAM_KEY="qpcb-3zs7-m3hj-ygse-6e0c"
+YOUTUBE_RTMP_URL="rtmp://a.rtmp.youtube.com/live2/${YOUTUBE_STREAM_KEY}"
+
+# ---------- Google Drive سے ویڈیو ڈاؤن لوڈ کریں ----------
+FILE_ID=$(echo "$GOOGLE_DRIVE_LINK" | grep -oP '(?<=/d/)[a-zA-Z0-9_-]+')
+
+echo "Google Drive سے ویڈیو ڈاؤن لوڈ ہو رہی ہے..."
+gdown --id "$FILE_ID" -O "$LOCAL_VIDEO_PATH"
+echo "ڈاؤن لوڈ مکمل: $LOCAL_VIDEO_PATH"
+
+# ---------- بیک وقت Facebook (horizontal) اور YouTube (vertical) پر لائیو ----------
+echo "لائیو اسٹریم شروع ہو رہی ہے..."
+
+ffmpeg -re -stream_loop -1 -i "$LOCAL_VIDEO_PATH" \
+  -filter_complex "[0:v]split=2[fb_v][yt_pre];[yt_pre]scale=1080:-2,crop=1080:1920[yt_v]" \
+  -map "[fb_v]" -map 0:a \
+  -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 60 \
+  -c:a aac -b:a 160k -ar 44100 \
+  -f flv "$FACEBOOK_RTMP_URL" \
+  -map "[yt_v]" -map 0:a \
+  -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k -pix_fmt yuv420p -g 60 \
+  -c:a aac -b:a 160k -ar 44100 \
+  -f flv "$YOUTUBE_RTMP_URL"
